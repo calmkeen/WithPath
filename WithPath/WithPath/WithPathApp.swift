db77e0b7074b759785a86f17059a792a0c390108
Younsung <78361650+calmkeen@users.noreply.github.com>
2026-04-26 16:30:54 +0900

    [ADD]: Basic Xcode project template

---
+++ WithPath/WithPath/WithPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  WithPathApp.swift
+//  WithPath
+//
+//  Created by calmkeen on 4/26/26.
+//
+
+import SwiftUI
+
+@main
+struct WithPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
